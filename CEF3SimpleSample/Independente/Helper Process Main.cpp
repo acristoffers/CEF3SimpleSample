13f934818a8ca574137c8b3669596608c40f1757
Álan Crístoffer <acristoffers@gmail.com>
2013-09-02 19:51:59 -0300

    Fixes year in licenses

--- CEF3SimpleSample/Independente/Helper Process Main.cpp
+++ CEF3SimpleSample/Independente/Helper Process Main.cpp
@@ -1,5 +1,5 @@
 /************************************************************************************************
- Copyright (c) 2012 Álan Crístoffer
+ Copyright (c) 2013 Álan Crístoffer
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
