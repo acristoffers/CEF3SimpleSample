56ec090121cf2666240623e9a180a0a67ea5b5c6
Álan Crístoffer <acristoffers@gmail.com>
2013-09-02 12:02:27 -0300

    Added CEF libraries. Compiles without warnings

---
+++ CEF3SimpleSample/Mac/AppDelegate.mm
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  CEF3SimpleSample
+//
+//  Created by Álan Crístoffer on 02.09.13.
+//  Copyright (c) 2013 Álan Crístoffer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@dynamic window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
