d866db58f6678a19d458826af8d53783105742a8
Álan Crístoffer <acristoffers@gmail.com>
2013-09-03 21:07:37 -0300

    Added Windows. Compiles, but doesn't run.
    When open it creates the various processes which opens various windows
    and then freezes.

---
+++ CEF3SimpleSample/Win/main.cpp
@@ -0,0 +1,193 @@
+#include <string>
+#include <windows.h>
+
+#include "include/cef_app.h"
+#include "include/cef_browser.h"
+
+#include "ClientApp.h"
+#include "ClientHandler.h"
+
+ClientHandler* g_handler = 0;
+
+std::string GetApplicationDir()
+{
+	HMODULE hModule = GetModuleHandleW(NULL);
+	WCHAR wpath[MAX_PATH];
+	GetModuleFileNameW(hModule, wpath, MAX_PATH);
+
+	std::wstring wide( wpath );
+	std::string path( wide.begin(), wide.end() );
+
+	return path.substr(0, path.find_last_of("\\/"));
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_SIZE:
+		if (g_handler) {
+			// Resize the browser window and address bar to match the new frame
+			// window size
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+
+			HDWP hdwp = BeginDeferWindowPos(1);
+			hdwp = DeferWindowPos(hdwp, g_handler->GetBrowserHwnd(), NULL,
+				rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top,
+				SWP_NOZORDER);
+			EndDeferWindowPos(hdwp);
+		}
+		break;
+
+	case WM_ERASEBKGND:
+		if (g_handler) {
+			// Dont erase the background if the browser window has been loaded
+			// (this avoids flashing)
+			return 0;
+		}
+		break;
+
+	case WM_CLOSE:
+		if (g_handler) {
+			CefRefPtr<CefBrowser> browser = g_handler->GetBrowser();
+			if (browser.get()) {
+				// Let the browser window know we are about to destroy it.
+				browser->GetHost()->ParentWindowWillClose();
+			}
+		}
+		break;
+
+	case WM_PAINT:
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		EndPaint(hwnd, &ps);
+		return 0;
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+HWND RegisterWindow(HINSTANCE hInstance, int nCmdShow)
+{
+	WNDCLASS wc = { };
+
+	wc.lpfnWndProc   = WindowProc;
+	wc.hInstance     = hInstance;
+	wc.lpszClassName = "TestsDatabaseWindow";
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(
+		0,                                      // Optional window styles.
+		"TestsDatabaseWindow",                  // Window class
+		"CEF3 Simple Sample",                   // Window text
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,  // Window style
+
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,       // Parent window    
+		NULL,       // Menu
+		hInstance,  // Instance handle
+		NULL        // Additional application data
+		);
+
+	if (hwnd == NULL)
+		return 0;
+
+	ShowWindow(hwnd, nCmdShow);
+
+	return hwnd;
+}
+
+LRESULT CALLBACK MessageWndProc(HWND hWnd, UINT message, WPARAM wParam,
+                                LPARAM lParam)
+{
+  return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+HWND CreateMessageWindow(HINSTANCE hInstance)
+{
+	WNDCLASSEX wc = {0};
+	wc.cbSize = sizeof(wc);
+	wc.lpfnWndProc = MessageWndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = "ClientMessageWindow";
+	RegisterClassEx(&wc);
+
+	return CreateWindow("ClientMessageWindow", 0, 0, 0, 0, 0, 0, HWND_MESSAGE, 0,
+		hInstance, 0);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+	// Register the window class.
+	HWND hwnd = RegisterWindow(hInstance, nCmdShow);
+	if( hwnd == 0 )
+		return 0;
+
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+
+	CefMainArgs main_args(hInstance);
+	CefRefPtr<ClientApp> app(new ClientApp);
+
+	// Execute the secondary process, if any.
+	int exit_code = CefExecuteProcess(main_args, app.get());
+	if (exit_code >= 0) {
+		exit(exit_code);
+	}
+
+	CefSettings settings;
+
+	CefInitialize(main_args, settings, app.get());
+
+	CefWindowInfo info;
+	CefBrowserSettings b_settings;
+	CefRefPtr<CefClient> client(new ClientHandler);
+
+	std::string path;
+
+	CefRefPtr<CefCommandLine> command_line = CefCommandLine::GetGlobalCommandLine();
+	if (command_line->HasSwitch("url")) {
+		path = command_line->GetSwitchValue("url");
+	}
+
+	if (path.empty()) {
+		path = GetApplicationDir();
+		path = "file://" + path + "/html/index.html";
+	}
+
+	info.SetAsChild(hwnd, rect);
+	CefBrowserHost::CreateBrowser(info, client.get(), path, b_settings);
+
+	int result = 0;
+	if (!settings.multi_threaded_message_loop) {
+		// Run the CEF message loop. This function will block until the application
+		// recieves a WM_QUIT message.
+		CefRunMessageLoop();
+	} else {
+		// Create a hidden window for message processing.
+		HWND hMessageWnd = CreateMessageWindow(hInstance);
+
+		MSG msg;
+
+		// Run the application message loop.
+		while (GetMessage(&msg, NULL, 0, 0)) {
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+		}
+
+		DestroyWindow(hMessageWnd);
+		hMessageWnd = NULL;
+
+		result = static_cast<int>(msg.wParam);
+	}
+	CefShutdown();
+
+	return result;
+}
