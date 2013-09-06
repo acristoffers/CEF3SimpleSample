bb22ca698f411ee42d0f63823de5b18a00d5ec47
Álan Crístoffer <acristoffers@gmail.com>
2013-09-06 11:10:11 -0300

    Add linux support

---
+++ CEF3SimpleSample/Linux/main.cpp
@@ -0,0 +1,119 @@
+﻿#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+
+#include <gtk/gtk.h>
+
+#include "include/cef_app.h"
+#include "include/cef_browser.h"
+#include "include/cef_frame.h"
+#include "include/cef_runnable.h"
+
+#include "ClientHandler.h"
+#include "ClientApp.h"
+
+void TerminationSignalHandler(int signatl)
+{
+    CefQuitMessageLoop();
+}
+
+static gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer data)
+{
+    CefShutdown();
+    return TRUE;
+}
+
+GdkPixbuf *create_pixbuf(const gchar *filename)
+{
+    GdkPixbuf *pixbuf;
+    GError    *error = NULL;
+
+    pixbuf = gdk_pixbuf_new_from_file(filename, &error);
+    if ( !pixbuf ) {
+        fprintf(stderr, "%s\n", error->message);
+        g_error_free(error);
+    }
+
+    return pixbuf;
+}
+
+std::string getExecutableBaseFolder()
+{
+    std::string path    = "";
+    pid_t       pid     = getpid();
+    char        buf[20] = {
+        0
+    };
+
+    sprintf(buf, "%d", pid);
+    std::string _link = "/proc/";
+    _link.append(buf);
+    _link.append("/exe");
+    char proc[512];
+    int  ch = readlink(_link.c_str(), proc, 512);
+    if ( ch != -1 ) {
+        proc[ch] = 0;
+        path     = proc;
+        std::string::size_type t = path.find_last_of("/");
+        path = path.substr(0, t);
+    }
+
+    return path;
+}
+
+int main(int argc, char **argv)
+{
+    CefMainArgs main_args(argc, argv);
+
+    CefRefPtr<ClientApp> app(new ClientApp);
+
+    // Execute the secondary process, if any.
+    int exit_code = CefExecuteProcess( main_args, app.get() );
+    if ( exit_code >= 0 ) {
+        exit(exit_code);
+    }
+
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+    g_signal_connect(window, "delete-event", G_CALLBACK(delete_event), NULL);
+
+    GtkWidget *vbox = gtk_vbox_new(FALSE, 0);
+
+    CefSettings settings;
+
+    CefInitialize( main_args, settings, app.get() );
+
+    CefWindowInfo        info;
+    CefBrowserSettings   b_settings;
+    CefRefPtr<CefClient> client(new ClientHandler);
+
+    std::string path     = getExecutableBaseFolder();
+    std::string app_icon = path + "/html/img/icon.png";
+    path = std::string("file://") + path + std::string("/html/index.html");
+
+    CefRefPtr<CefCommandLine> command_line = CefCommandLine::GetGlobalCommandLine();
+    if ( command_line->HasSwitch("url") ) {
+        path = command_line->GetSwitchValue("url");
+    }
+
+    info.SetAsChild(vbox);
+
+    CefBrowserHost::CreateBrowser(info, client.get(), path, b_settings);
+
+    gtk_window_set_icon( GTK_WINDOW(window), create_pixbuf( app_icon.c_str() ) );
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+    gtk_widget_show_all( GTK_WIDGET(window) );
+
+    signal(SIGINT,  TerminationSignalHandler);
+    signal(SIGTERM, TerminationSignalHandler);
+
+    CefRunMessageLoop();
+
+    return 0;
+}
---
+++ build-linux.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+mkdir -p bin # for silencing the next line, in case the folder does not exist
+rm -r bin
+mkdir -p build
+touch build/touched # for silencing the next line, in case the folder was just created
+rm -r build/*
+cd build
+
+echo "Building CEF3SimpleSample::Independente"
+g++ -c ../CEF3SimpleSample/Independente/*.cpp \
+	-I ../CEF3SimpleSample/cc \
+	-I ../CEF3SimpleSample/CEF/Linux \
+	-I ../CEF3SimpleSample/CEF/Linux/include \
+	`pkg-config --cflags --libs gtk+-2.0`
+
+rm "Helper Process Main.o"
+
+echo "Building CEF3SimpleSample::Linux"
+g++ -c ../CEF3SimpleSample/Linux/*.cpp \
+	-I ../CEF3SimpleSample/Independente \
+	-I ../CEF3SimpleSample/CEF/Linux \
+	-I ../CEF3SimpleSample/CEF/Linux/include \
+	`pkg-config --cflags --libs gtk+-2.0`
+
+echo "Linking..."
+g++ *.o \
+	-o CEF3SimpleSample \
+	`pkg-config --cflags --libs gtk+-2.0` \
+	-L ../CEF3SimpleSample/CEF/Linux/lib \
+	-ldl \
+	-lcef_dll_wrapper \
+	-lcef \
+	-Wl,-R. -Wl,-R/usr/lib
+
+echo "Copying files around..."
+rm *.o
+mkdir bin
+mv CEF3SimpleSample bin/
+cp -r ../CEF3SimpleSample/CEF/Linux/Resources/* bin/
+cp -r ../CEF3SimpleSample/CEF/Linux/lib/*.so bin/
+cp -r ../CEF3SimpleSample/Independente/html bin/html
+mv bin ..
+cd ..
+rm -r build
+
