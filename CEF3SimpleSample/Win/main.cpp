2b1592f76039a51b5a457d19cca6130155289fa4
Álan Crístoffer <acristoffers@gmail.com>
2013-09-07 13:19:16 -0300

    Add disclamer notice to Linux/main.cpp

--- CEF3SimpleSample/Win/main.cpp
+++ CEF3SimpleSample/Win/main.cpp
@@ -15,6 +15,7 @@
 *   OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 *   DEALINGS IN THE SOFTWARE.
 ************************************************************************************************/
+
 #include <string>
 #include <algorithm>
 #include <windows.h>
